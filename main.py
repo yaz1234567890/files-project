50d444a55b7deeb84c932bf7016a52feba157a78
yazzi <yasmine_fisher@biswashington.org>
2025-01-16 17:50:01 +0000

    im dine

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+with open("students.txt", "r+") as file:
+    for line in file:
+        print(line.strip())
 
+with open("students.txt", "r+") as file:
+    for line in file:
+        print (line[2])
+
+with open("students.txt", "r+") as file:
+    for line in file:
+        if len(line.strip()) > 6:
+            print(line.strip())
+
+new_student= input("enter a new name")
+
+with open ("students.txt", "a") as file:
+    file.write(new_student + "\n")
